e54f76900bf8edd2e5a5b84d04954ab0e707eccb
diveshpremchandani <diveshpremchandani@gmail.com>
2025-06-20 03:09:49 +0530

    Updated test cases and config for PAN verification

--- tests/authbri.spec.js
+++ tests/authbri.spec.js
@@ -13,7 +13,7 @@ test('test', async ({ page }) => {
   await expect(page).toHaveURL('https://authbridge.com/checks/pan-card-verification/');
   await expect(page).toHaveTitle('PAN Card Verification API | PAN Card Verification - AuthBridge'); // Assumes the title contains 'PAN Card Verification'
     await expect(page.locator('h1')).toHaveText('PAN Card Verification API');
-    //await expect(page.locator('p')).toContainText('PAN Card Verification API is a service that allows you to verify the authenticity of PAN cards in India. It helps businesses and individuals ensure that the PAN card details provided by users are valid and match the official records.');
+    ///await expect(page.locator('p')).toContainText('PAN Card Verification API is a service that allows you to verify the authenticity of PAN cards in India. It helps businesses and individuals ensure that the PAN card details provided by users are valid and match the official records.');
     //await expect(page.locator('h2')).toHaveText('Key Features');
     await page.locator("//div[@class='elementor-element elementor-element-4ac33731 elementor-widget elementor-widget-heading']//h4[@class='elementor-heading-title elementor-size-default'][normalize-space()='Read more >>']").click()
     await expect(page).toHaveURL('https://authbridge.com/blog/voter-id-verification-for-user-onboarding/');
